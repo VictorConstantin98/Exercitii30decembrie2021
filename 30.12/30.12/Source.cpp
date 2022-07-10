32d405df02914c8d8e057b08823372f5470066c9
Costi <sova.victor98@gmail.com>
2022-07-10 18:35:36 +0300

    Exercitii din 30 decembrie 2021

---
+++ 30.12/30.12/Source.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+void main()
+{
+	//Sa se afiseze primele 10 nr naturale
+	/*for (int i = 0; i < 10; i++)
+	{
+		cout << i << endl;
+	}*/
+	
+	//Sa se afiseze primele 10 nr naturale de la coada la cap
+	/*for (int i = 10; i > 0; i--)
+	{
+		cout << i << endl;
+	}*/
+
+	//Sa se calculeze suma primelor 10 numere naturale
+	//int suma = 0;
+	//for (int i = 0; i < 10; i++)
+	//{
+	//	cout << i << endl;
+	//	suma = suma + i; //suma+=i;
+	//}
+	//cout << "Suma este egala cu " << suma << endl;
+
+	//Homework
+	//Afisam numerele pare naturale mai mici decat 100
+	//Afiseaza primele 10 numere din sirul lui Fibonacci
+
+	//Ex 1 
+	/*for (int i = 0; i < 100; i++)
+	{
+		if (i % 2 == 0)
+		{
+			cout << i << endl;
+		}
+	}*/
+
+	//Ex 2
+	//cout << "Primele 10 numere din sirul lui Fibonacci sunt: " << endl;
+	//int n1 = 0;
+	//int n2 = 1;
+	//int n;
+	//cout << n1 << endl;
+	//cout << n2 << endl;
+	//for (int i = 2; i < 10; i++)
+	//{
+	//	n = n1 + n2;			//n=1;
+	//	n1 = n2;				//n1=1;  //i=2<10=>continua
+	//	n2 = n;					//n2=1; 
+	//	cout << n << endl;		
+	//}
+
+
+	
+	
+
+
+
+
+
+
+
+
+
+}
